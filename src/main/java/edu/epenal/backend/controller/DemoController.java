e6cdc43cb4f0373b2050cc1bab114f24660141e7
Maribel Cruz Téllez <maribel.cruz@lucasianmexico.com>
2024-08-01 16:42:42 -0600

    kubernetes

--- src/main/java/edu/epenal/backend/controller/DemoController.java
+++ src/main/java/edu/epenal/backend/controller/DemoController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class DemoController {
 
@@ -13,7 +16,16 @@ public class DemoController {
     private String message;
 
     @GetMapping
-    public ResponseEntity<MessageDTO> getMessage(){
-        return ResponseEntity.ok(new MessageDTO("Welcome to %s!!!".formatted(message)));
+    public ResponseEntity<MessageDTO> getMessage() {
+
+        try {
+            InetAddress addr;
+            addr = InetAddress.getLocalHost();
+            return ResponseEntity.ok(new MessageDTO("Welcome to %s!!!".formatted(addr.getHostName())));
+        } catch (UnknownHostException ex) {
+            return ResponseEntity.ok(new MessageDTO("Welcome to %s!!!".formatted(message)));
+        }
+
+
     }
 }
